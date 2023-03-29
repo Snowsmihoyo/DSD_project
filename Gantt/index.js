50e71d2a900b9c21a793b59e04587c34e355e541
struartsnow <43714586+struartsnow@users.noreply.github.com>
2023-03-29 16:47:57 +0800

    Add files via upload

---
+++ Gantt/index.js
@@ -0,0 +1,62 @@
+var menulist = [
+    [{name: "Home", src: "index.html"}],
+    [
+        {name: "News", src: ""}, 
+        {name: "Website Construction Record", src: "docs/Website Construction Record.html"},
+        {name: "Research on end-to-end latency between different devices", src: "docs/Research on end-to-end latency between different devices.html"}, 
+        {name: "BlogForWeek1", src: "docs/BlogForWeek1.html"}, 
+        {name: "More", src: "docs/news.html"}
+    ],[
+        {name: "Documents", src: ""}, 
+        {name: "Software Requirements Specification (SRS)", src: "docs/Software Requirements Specification (SRS).html"},
+        {name: "End-to-end latency between different devices", src: "docs/End-to-end latency between different devices.html"}, 
+        {name: "GDSS", src: "docs/GDSS.html"}, {name: "More", src :"docs/documents.html"}
+    ],
+    [{name: "Members", src: "docs/member.html"}],
+    [{name: "Borrowing", src: "docs/borrow.html"}],
+    [{name: "Online", src: "docs/realtimedata.html"}]
+]
+//{name: "", src: "docs/.html"},
+var caret = '<span class="caret"></span>'
+var presentation_normal = '<li class="nav-item">'
+var presentation_dropdown = '<li class="nav-item dropdown">'
+var dropdown_toggle_1 = '<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="'
+var dropdown_toggle_2 =  '" role="button" aria-expanded="false">'
+var dropdown_menu = '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">'
+var toptext = `
+<div class="navbar-brand">PIrates-Raspberry Pi</div>
+<button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+    data-bs-target="#collapsed-nav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsed-nav">
+    <ul class="navbar-nav">`
+
+function isNormalPage(flag) {
+    if(flag == 0) {
+        return ""
+    } else {
+        return "../"
+    }
+}
+
+function topbar(flag) {
+    var text = ""
+    for(var i = 0; i < menulist.length; i++) {
+        var rlength = menulist[i].length
+        if(rlength == 1) {
+            text = text + presentation_normal + '<a class="nav-link" href="' + isNormalPage(flag) + menulist[i][0].src + '">' + menulist[i][0].name + '</a></li>'
+        }
+        else {
+            text = text + presentation_dropdown + dropdown_toggle_1 + isNormalPage(flag) + menulist[i][0].src + dropdown_toggle_2 + menulist[i][0].name + caret + '</a>' + dropdown_menu
+            for(var j = 1; j < rlength; j++) {
+                text = text + '<li><a class="dropdown-item" href="' + isNormalPage(flag) + menulist[i][j].src + '">' + menulist[i][j].name + "</a></li>"
+            }
+            text = text + "</ul></li>"
+        }
+        
+    }
+    text = text + `</ul></ul></div>`
+    console.log(text)
+    document.getElementById("top").innerHTML = toptext + text
+}
---
+++ Gantt/pages.js
@@ -0,0 +1,53 @@
+function readTextFile(filePath, callback) {
+    const xhrFile = new XMLHttpRequest();
+    xhrFile.open("GET", filePath, true);
+    xhrFile.onload  = function() {
+        const allText = xhrFile.response;
+        callback(allText)
+    }
+    xhrFile.send();
+}
+
+function loadList(num) {
+    var text = ``
+    readTextFile("../js/information.json", (textDetail) => {
+        TmpList = JSON.parse(textDetail)
+        List = TmpList[num]
+        //console.log(List)
+        for(var i = 0; i < List.length; i++) {
+            text = text + `<div class="day">` + 
+            `<div class="postTitle"><a href="` + List[i].src + `">` + List[i].title + `</a></div>` +
+            `<span class="postMeta">Posted on ` + List[i].time + `</span>`+ 
+            `<div class="postCon"><div class="mainText">` + List[i].abstract + `</div><a href="` + List[i].src + `">Read more.</a></div>`+ 
+            `</div>`
+        }
+        //console.log(text)
+        document.getElementById("articlelist").innerHTML = text
+    })
+}
+
+function loadTitle() {
+    var text = ``
+    console.log("HERE")
+    readTextFile("js/information.json", (textDetail) => {
+        TmpList = JSON.parse(textDetail)
+        newsList = TmpList[0]
+        docsList = TmpList[1]
+        var num = 0
+        for(var i = newsList.length - 1; i >= 0; i--) {
+            text = text + `<p><span class="new">NEW!</span><a href="docs/` + newsList[i].src + `">` + newsList[i].title + `</a></p>`
+            num++
+            if(num >= 5) break;
+        }
+        document.getElementById("newstext").innerHTML = text
+        text = ``
+        num = 0
+        for(var i = docsList.length - 1; i >= 0; i--) {
+            text = text + `<p><span class="new">NEW!</span><a href="docs/` + docsList[i].src + `">` + docsList[i].title + `</a></p>`
+            num++
+            if(num >= 5) break;
+        }
+        document.getElementById("docstext").innerHTML = text
+        
+    })
+}
